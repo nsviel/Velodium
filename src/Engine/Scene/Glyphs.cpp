a1aa7912eb3b6bd5282fece3cf0bbfd11e4ca63f
nsviel <nathan.sanchizviel@pm.me>
2022-03-07 11:31:17 +0100

    commit

--- src/Engine/Scene/Glyphs.cpp
+++ src/Engine/Scene/Glyphs.cpp
@@ -153,7 +153,7 @@ void Glyphs::runtime_glyph_subset(Subset* subset){
 void Glyphs::runtime_glyph_pred(Cloud* cloud, int subset_ID){
   Scene* sceneManager = node_engine->get_sceneManager();
   Subset* subset_selected = sceneManager->get_subset_selected();
-  if(subset_ID != subset_selected->ID && subset_ID == 0) return;
+  if(subset_ID != subset_selected->ID || subset_ID == 0) return;
   //---------------------------
 
   Subset* subset = sceneManager->get_subset_byID(cloud, subset_ID - 1);
