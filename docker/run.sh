53fe8c2382b356ee475fe03cc1e3278cacac7f45
nsviel <nathan.sanchizviel@pm.me>
2022-03-29 17:33:48 +0200

    commit

--- docker/run.sh
+++ docker/run.sh
@@ -2,9 +2,14 @@
 
 #export DISPLAY=:0
 
-docker run -it \
-    --gpus all \
+# Allow X server connection
+xhost +local:root
+
+docker run -it --rm \
     --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     velodium
 
+# Disallow X server connection
+xhost -local:root
